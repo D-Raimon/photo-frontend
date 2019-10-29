0cef82cd5b170f9a0fe85be0ea4bfa8f18be074a
D-Raimon <raimon67@gmail.com>
2019-10-29 16:34:31 -0400

    Change heroku url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://blooming-cliffs-96004.herokuapp.com',
+  production: 'https://serene-crag-70106.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
